ccbcb81df0b8fc1b0ef556771505a81d9a6f9682
PI299574 <kumarpintu1305@gmail.com>
2016-07-11 04:39:44 +0000

    fetching data

--- js/searchBox.js
+++ js/searchBox.js
@@ -1,75 +1,105 @@
-var Searchbox=React.createClass({
+var ResultItems=React.createClass({
   render:function(){
+    console.log(this.props.Director);
     return(
-      <div id="searchbox">
-      <form className="" onSubmit={this.loadMovieData}>
-      <input type="text" id="search" placeholder="Search Your movie" />
-        <Buttonbox />
-        </form>
-      </div>
-    );
-  }
-});
-
-var Buttonbox=React.createClass({
-  render:function(){
-    return(
-      <input type="submit" value="submit" id="button" />
+      //<li>{this.props.searchResults.response.Director}</li>
+<li> {this.props.Director}</li>
     );
   }
 });
 
 var Result=React.createClass({
   render:function(){
-    return(
-      <ul>ResultItems</ul>
-    );
+console.log("L12 type is " + typeof this.props.searchResults);
+console.log("L13 string is " + JSON.stringify(this.props.searchResults));
+
+  var resultItems=this.props.searchResults.map(function(result){
+console.log("L16 " + result.imdbID + " "+ result.Director );
+    return <ResultItems key={result.imdbID} Director={result.Director} />
+
+    });
+
+console.log("L18 "+ {resultItems});
+return(<div>{resultItems}</div>);
   }
 });
 
-var ResultItems=React.createClass({
-  render:function(){
-    return(
-      <li>display Result Here</li>
-    );
-  }
-});
+
 
 var MovieApp=React.createClass({
 getInitialState:function(){
   return{
-    searchresults:[]
+    searchResults:[]
   };
 },
 showResults:function(response){
+  console.log(response);
   this.setState({
-    searchresults:response.results
-  })
-  console.log(response)
+
+    searchResults:[response]
+  });
 },
 
 search:function(url){
-  console.log(url);
-  $.ajax({
+$.ajax({
     type:"GET",
-    dataType:'jsonp',
-    url:url,
-    success: function(response){
-      this.showResults(response);
-    }.bind(this)
-  });
+     dataType:'jsonp',
+     url:url,
+     success: function(response){
+                this.showResults(response);
+              }.bind(this)
+   });
 },
-componentDidMount(){
-   this.search('https://itunes.apple.com/search?term=fun');
-},
+
   render:function(){
   return(  <div>
-    <Searchbox />
-    <Result />
+    <Searchbox search={this.search}/>
+    <Result searchResults={this.state.searchResults} />
     </div>
   );
   }
 });
+
+
+var Searchbox=React.createClass({
+
+  createAjax:function(e){
+    //alert("ajax calling");
+    e.preventDefault();
+    //console.log(this);
+    var query=ReactDOM.findDOMNode(this.refs.query).value;
+    //var query="sultan";
+    var year=ReactDOM.findDOMNode(this.refs.year).value;
+    //var year="2016";
+  var category = ReactDOM.findDOMNode(this.refs.category).value;
+  //  var category="full";
+    var url='http://www.omdbapi.com/?t='+query+'&y='+year+'&plot='+category+'&r=json';
+//var url='https://itunes.apple.com/search?term=fun';
+    this.props.search(url)
+
+  },
+  handleChange: function() {
+  },
+  render:function(){
+
+    return(
+      <div id="searchbox">
+      <form className=""  onSubmit={this.createAjax}>
+      <input type="text" ref="query" placeholder="Search Your movie"  />
+      <input type="text" ref="year" placeholder="Year" />
+      <select ref="category">
+                    <option value="short">short</option>
+                    <option value="full">full</option>
+                </select>
+        <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
+
+});
+
+
 ReactDOM.render(
   <MovieApp/>,
   document.getElementById('content')
